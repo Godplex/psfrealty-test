408f239f9c408a71083150e73c98b8ba5dc85426
Julian Rincon <julian1918@hotmail.es>
2022-11-11 08:31:24 -0500

    Prueba 2

--- components/Map.js
+++ components/Map.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Map = () => {
     return (
         <iframe
-            class="w-100 pt-5 px-4"
+            className="w-100 pt-5 px-4"
             src="https://maps.google.com/?ll=23.135249,-82.359685&z=14&t=m&output=embed"
             height="70%"
             frameBorder="0"
-            allowfullscreen>
+            allowFullScreen>
         </iframe>
     )
 }
--- pages/property/buy/city/[name].js
+++ pages/property/buy/city/[name].js
@@ -1,67 +1,52 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import HomeItems from '../../../../components/HomeItems';
-import { server } from './../../../../config/index';
 import Map from './../../../../components/Map';
+import useSWR from 'swr';
 
-export async function getServerSideProps(context) {
+// Define a custom fetcher function
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
-    const { name } = context.query;
-
-    const res = await fetch(`${server}/api/property/buy/city/${name}`);
-    const properties = await res.json();
-
-    return { props: { properties: properties } }
-}
-
-const name = ({ properties }) => {
-
-    const [listProperty, setListProperty] = useState([]);
+const name = () => {
 
     const router = useRouter();
     const { name } = router.query;
 
-    const [nameWithSpaces, setnameWithSpaces] = useState('');
-
-    useEffect(() => {
-
-        setListProperty(properties);
-
-        if (name) {
-            setnameWithSpaces(name.replaceAll('-', ' '));
-        }
-
-    }, [name])
+    const { data } = useSWR(`/api/property/buy/city/${name}`, fetcher)
 
     return (
         <div className='container-fluid'>
             <div className='row gx-5 m-0'>
                 <div className='col-md-6'>
-                    <div>
-                        {
-                            name
-                            &&
-                            <p className='text-center fw-bold pt-4'>Casa y apartamentos en venta en {nameWithSpaces}</p>
-                        }
-                        <div className='d-flex justify-content-between'>
-                            <div>
+                    {
+                        data
+                        &&
+                        <div>
+                            {
+                                name
+                                &&
+                                <p className='text-center fw-bold pt-4'>Casa y apartamentos en venta en {name.replaceAll('-', ' ')}</p>
+                            }
+                            <div className='d-flex justify-content-between'>
+                                <div>
+                                    {
+                                        data.length == 1
+                                            ?
+                                            <p>{data.length} propiedad encontrada</p>
+                                            :
+                                            <p>{data.length} propiedades encontradas</p>
+                                    }
+                                </div>
+                            </div>
+                            <div className='row gy-4 py-4'>
                                 {
-                                    listProperty.length == 1
-                                        ?
-                                        <p>{listProperty.length} propiedad encontrada</p>
-                                        :
-                                        <p>{listProperty.length} propiedades encontradas</p>
+                                    data.map((e, index) => {
+                                        return <HomeItems key={index} {...e} />
+                                    })
                                 }
                             </div>
                         </div>
-                        <div className='row gy-4 py-4'>
-                            {
-                                listProperty.map((e, index) => {
-                                    return <HomeItems key={index} {...e} />
-                                })
-                            }
-                        </div>
-                    </div>
+                    }
                 </div>
                 <div className="col-md-6 d-md-block d-none">
                     <Map />
